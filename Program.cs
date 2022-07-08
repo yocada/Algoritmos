b6bfce96a454daf46c8d8251171ac3d9a447756d
DESKTOP-D2JVQPV\dcort <dcortesmut@gmail.com>
2022-07-09 00:25:09 +0200

    Creación del proyecto

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using EstructurasDatosAvanzadas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var grafoDirigido = new GrafoDirigido<int>();
+            for (int i = 1; i <= 7; i++)
+            {
+                grafoDirigido.addVertice(i);
+            }
+            grafoDirigido.addArista(1, 2, 10);
+            grafoDirigido.addArista(1, 5, 6);
+            grafoDirigido.addArista(2, 4, 2);
+            grafoDirigido.addArista(2, 3, 5);
+            grafoDirigido.addArista(3, 4, 4);
+            grafoDirigido.addArista(4, 1, 5);
+            grafoDirigido.addArista(5, 6, 9);
+            grafoDirigido.addArista(5, 7, 1);
+            grafoDirigido.addArista(6, 3, 2);
+            grafoDirigido.addArista(6, 4, 3);
+            grafoDirigido.addArista(6, 7, 8);
+            var tupla = Voraces.Dijkstra(grafoDirigido, 1);
+            System.Diagnostics.Debug.WriteLine(Voraces.MostrarCaminoDijkstra(tupla, 1, 7));
+        }
+    }
+}
